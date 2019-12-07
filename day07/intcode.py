c67b2623757785ff12a733d503525bbc066a6f52
Dave Page <dave@davepage.me.uk>
2019-12-07 23:15:49 +0000

    - Refactor with object orientation ahead of part 2
    - New class IntCode() with internal state and instruction pointer
    
    - You want to diff this ignoring whitespace for sensible changes

--- day07/intcode.py
+++ day07/intcode.py
@@ -3,172 +3,178 @@
 import argparse
 from itertools import permutations
 
-def decode_argument( program, argument, mode ):
-    if args.decode:
-        print("\tDecoding argument", argument, "with mode", mode)
+class IntCode:
 
-    if mode == 0:
+    def __init__( self, program ):
+        self.program = program
+        self.pos = 0
+
+    def decode_argument(self, argument, mode):
         if args.decode:
-            print("\tPosition mode;", argument, "is an address containing", program[argument])
-        return( program[argument] )
-    else:
-        if args.decode:
-            print("\tImmediate mode;", argument, "is a value")
-        return( argument ) 
+            print("\tDecoding argument", argument, "with mode", mode)
 
-def do_add(program, pos, modes):
-    if args.verbose:
-        print("\t", program[pos:pos+4])
-    arg_a = decode_argument( program, program[pos+1], modes[0] )
-    arg_b = decode_argument( program, program[pos+2], modes[1] )
-    pos_t = program[pos+3]
-    if args.verbose:
-        print("\tSum values", arg_a, arg_b, "into", pos_t)
-    program[pos_t] = arg_a + arg_b
-    return( [0, 4] )
-
-def do_mult(program, pos, modes):
-    if args.verbose:
-        print("\t", program[pos:pos+4])
-    arg_a = decode_argument( program, program[pos+1], modes[0] )
-    arg_b = decode_argument( program, program[pos+2], modes[1] )
-    pos_t = program[pos+3]
-    if args.verbose:
-        print("\tMultiply values", arg_a, arg_b, "into", pos_t)
-    program[pos_t] = arg_a * arg_b
-    return( [0, 4] )
-
-def do_input(program, pos, inputdata):
-    if args.verbose:
-        print("\t", program[pos:pos+2])
-    arg_a = program[pos+1]
-
-    if len(inputdata) == 0:
-        print("Want to receive input but we have none!")
-        exit(1)
-
-    value = inputdata.pop(0)
-    if args.verbose:
-        print("\tStoring input value", value, "at location", arg_a)
-    program[arg_a] = value
-
-    return( [0, 2] )
-
-def do_output(program, pos, modes, output):
-    if args.verbose:
-        print("\t", program[pos:pos+2])
-    arg_a = decode_argument( program, program[pos+1], modes[0] )
-
-    output.append(arg_a)
-
-    return( [0, 2] )
-
-def do_jit(program, pos, modes):
-    if args.verbose:
-        print("\t", program[pos:pos+3])
-    arg_a = decode_argument( program, program[pos+1], modes[0] )
-    arg_b = decode_argument( program, program[pos+2], modes[1] )
-
-    if arg_a != 0:
-        if args.verbose:
-            print("\t", arg_a, "is nonzero, jumping to", arg_b)
-        return([1, arg_b])
-    else:
-        if args.verbose:
-            print("\t", arg_a, "is zero, doing nothing")
-        return([0, 3])
-
-def do_jif(program, pos, modes):
-    if args.verbose:
-        print("\t", program[pos:pos+3])
-    arg_a = decode_argument( program, program[pos+1], modes[0] )
-    arg_b = decode_argument( program, program[pos+2], modes[1] )
-
-    if arg_a == 0:
-        if args.verbose:
-            print("\t", arg_a, "is zero, jumping to", arg_b)
-        return([1, arg_b])
-    else:
-        if args.verbose:
-            print("\t", arg_a, "is nonzero, doing nothing")
-        return([0, 3])
-
-def do_lt(program, pos, modes):
-    if args.verbose:
-        print("\t", program[pos:pos+3])
-    arg_a = decode_argument( program, program[pos+1], modes[0] )
-    arg_b = decode_argument( program, program[pos+2], modes[1] )
-    arg_c = program[pos+3]
-
-    if arg_a < arg_b:
-        if args.verbose:
-            print("\t", arg_a, "is less than", arg_b, "; storing 1 at", arg_c)
-        program[arg_c] = 1
-    else:
-        if args.verbose:
-            print("\t", arg_a, "is not less than", arg_b, "; storing 0 at", arg_c)
-        program[arg_c] = 0
-
-    return([0, 4])
-
-def do_eq(program, pos, modes):
-    if args.verbose:
-        print("\t", program[pos:pos+3])
-    arg_a = decode_argument( program, program[pos+1], modes[0] )
-    arg_b = decode_argument( program, program[pos+2], modes[1] )
-    arg_c = program[pos+3]
-
-    if arg_a == arg_b:
-        if args.verbose:
-            print("\t", arg_a, "is equal to", arg_b, "; storing 1 at", arg_c)
-        program[arg_c] = 1
-    else:
-        if args.verbose:
-            print("\t", arg_a, "is not equal to", arg_b, "; storing 0 at", arg_c)
-        program[arg_c] = 0
-
-    return([0, 4])
-
-def compute(program, inputdata):
-    pos=0
-    output = []
-    while(1):
-        if args.verbose:
-            print("# Looking for opcode at", pos, ":", program[pos])
-
-        opcode = program[pos] % 100
-        modes = [ ( program[pos] // dec ) % 10 for dec in [ 100, 1000, 10000 ] ]
-
-        if opcode == 99:
-            return(output)
-        elif opcode == 1:
-            inst_len = do_add(program, pos, modes)
-        elif opcode == 2:
-            inst_len = do_mult(program, pos, modes)
-        elif opcode == 3:
-            inst_len = do_input(program, pos, inputdata)
-        elif opcode == 4:
-            inst_len = do_output(program, pos, modes, output)
-        elif opcode == 5:
-            inst_len = do_jit(program, pos, modes)
-        elif opcode == 6:
-            inst_len = do_jif(program, pos, modes)
-        elif opcode == 7:
-            inst_len = do_lt(program, pos, modes)
-        elif opcode == 8:
-            inst_len = do_eq(program, pos, modes)
+        if mode == 0:
+            if args.decode:
+                print("\tPosition mode;", argument, "is an address containing", self.program[argument])
+            return( self.program[argument] )
         else:
-            print("Unrecognised opcode", opcode, "at position", pos)
-            print(program)
+            if args.decode:
+                print("\tImmediate mode;", argument, "is a value")
+            return( argument ) 
+
+    def do_add(self, modes):
+        if args.verbose:
+            print("\t", self.program[self.pos:self.pos+4])
+        arg_a = self.decode_argument( self.program[self.pos+1], modes[0] )
+        arg_b = self.decode_argument( self.program[self.pos+2], modes[1] )
+        pos_t = self.program[self.pos+3]
+        if args.verbose:
+            print("\tSum values", arg_a, arg_b, "into", pos_t)
+        self.program[pos_t] = arg_a + arg_b
+        return( [0, 4] )
+
+    def do_mult(self, modes):
+        if args.verbose:
+            print("\t", self.program[self.pos:self.pos+4])
+        arg_a = self.decode_argument( self.program[self.pos+1], modes[0] )
+        arg_b = self.decode_argument( self.program[self.pos+2], modes[1] )
+        pos_t = self.program[self.pos+3]
+        if args.verbose:
+            print("\tMultiply values", arg_a, arg_b, "into", pos_t)
+        self.program[pos_t] = arg_a * arg_b
+        return( [0, 4] )
+
+    def do_input(self):
+        arg_a = self.program[self.pos+1]
+
+        if len(self.inputdata) == 0:
+            print("Want to receive input but we have none!")
             exit(1)
 
-        if inst_len[0] == 0:    # We've returned a relative jump for pos
-            pos += inst_len[1]
-        elif inst_len[0] == 1:  # Absolute position
-            pos = inst_len[1]
+        value = self.inputdata.pop(0)
+        if args.verbose:
+            print("\tStoring input value", value, "at location", arg_a)
+        self.program[arg_a] = value
+
+        return( [0, 2] )
+
+    def do_output(self, modes):
+        if args.verbose:
+            print("\t", self.program[self.pos:self.pos+2])
+        arg_a = self.decode_argument( self.program[self.pos+1], modes[0] )
+
+        self.output.append(arg_a)
+
+        return( [0, 2] )
+
+    def do_jit(self, modes):
+        if args.verbose:
+            print("\t", self.program[self.pos:self.pos+3])
+        arg_a = self.decode_argument( self.program[self.pos+1], modes[0] )
+        arg_b = self.decode_argument( self.program[self.pos+2], modes[1] )
+
+        if arg_a != 0:
+            if args.verbose:
+                print("\t", arg_a, "is nonzero, jumping to", arg_b)
+            return([1, arg_b])
         else:
-            print("Unexpected return type", inst_len[0])
-            exit(1)
+            if args.verbose:
+                print("\t", arg_a, "is zero, doing nothing")
+            return([0, 3])
+
+    def do_jif(self, modes):
+        if args.verbose:
+            print("\t", self.program[self.pos:self.pos+3])
+        arg_a = self.decode_argument( self.program[self.pos+1], modes[0] )
+        arg_b = self.decode_argument( self.program[self.pos+2], modes[1] )
+
+        if arg_a == 0:
+            if args.verbose:
+                print("\t", arg_a, "is zero, jumping to", arg_b)
+            return([1, arg_b])
+        else:
+            if args.verbose:
+                print("\t", arg_a, "is nonzero, doing nothing")
+            return([0, 3])
+
+    def do_lt(self, modes):
+        if args.verbose:
+            print("\t", self.program[self.pos:self.pos+3])
+        arg_a = self.decode_argument( self.program[self.pos+1], modes[0] )
+        arg_b = self.decode_argument( self.program[self.pos+2], modes[1] )
+        arg_c = self.program[self.pos+3]
+
+        if arg_a < arg_b:
+            if args.verbose:
+                print("\t", arg_a, "is less than", arg_b, "; storing 1 at", arg_c)
+            self.program[arg_c] = 1
+        else:
+            if args.verbose:
+                print("\t", arg_a, "is not less than", arg_b, "; storing 0 at", arg_c)
+            self.program[arg_c] = 0
+
+        return([0, 4])
+
+    def do_eq(self, modes):
+        if args.verbose:
+            print("\t", self.program[self.pos:self.pos+3])
+        arg_a = self.decode_argument( self.program[self.pos+1], modes[0] )
+        arg_b = self.decode_argument( self.program[self.pos+2], modes[1] )
+        arg_c = self.program[self.pos+3]
+
+        if arg_a == arg_b:
+            if args.verbose:
+                print("\t", arg_a, "is equal to", arg_b, "; storing 1 at", arg_c)
+            self.program[arg_c] = 1
+        else:
+            if args.verbose:
+                print("\t", arg_a, "is not equal to", arg_b, "; storing 0 at", arg_c)
+            self.program[arg_c] = 0
+
+        return([0, 4])
+
+    def compute(self, inputdata):
+        self.pos=0
+        self.inputdata = inputdata
+        self.output = []
+
+        while(1):
+            if args.verbose:
+                print("# Looking for opcode at", self.pos, ":", self.program[self.pos])
+
+            opcode = self.program[self.pos] % 100
+            modes = [ ( self.program[self.pos] // dec ) % 10 for dec in [ 100, 1000, 10000 ] ]
+
+            if opcode == 99:
+                return(self.output)
+            elif opcode == 1:
+                inst_len = self.do_add(modes)
+            elif opcode == 2:
+                inst_len = self.do_mult(modes)
+            elif opcode == 3:
+                inst_len = self.do_input()
+            elif opcode == 4:
+                inst_len = self.do_output(modes)
+            elif opcode == 5:
+                inst_len = self.do_jit(modes)
+            elif opcode == 6:
+                inst_len = self.do_jif(modes)
+            elif opcode == 7:
+                inst_len = self.do_lt(modes)
+            elif opcode == 8:
+                inst_len = self.do_eq(modes)
+            else:
+                print("Unrecognised opcode", opcode, "at position", self.pos)
+                print(self.program)
+                exit(1)
+
+            if inst_len[0] == 0:    # We've returned a relative jump for pos
+                self.pos += inst_len[1]
+            elif inst_len[0] == 1:  # Absolute position
+                self.pos = inst_len[1]
+            else:
+                print("Unexpected return type", inst_len[0])
+                exit(1)
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser()
@@ -189,9 +195,10 @@ for line in inputfile:
 
         signal = 0
         for phase in perm:
+            computer = IntCode(program.copy())
             if args.verbose:
                 print("\tUsing phase setting", phase, "and input signal", signal)
-            signal = compute(program.copy(), [phase, signal])[0]
+            signal = computer.compute([phase, signal])[0]
             if args.verbose:
                 print("Output from this instance", signal)
 
