4e746c3bbced87741a25303b800185268304cccf
Dave Page <dave.page@codethink.co.uk>
2019-12-10 12:22:38 +0000

    Break IntCode class into separate module

--- day09/intcode.py
+++ day09/intcode.py
@@ -1,33 +1,39 @@
 #!/usr/bin/python3
 
-import argparse
-from itertools import permutations
 import numpy
 
 class IntCode:
 
-    def __init__( self, program ):
+    def __init__( self, program, args ):
         self.program = numpy.zeros( (100000), dtype=int)
         for i in range(len(program)):
             self.program[i] = program[i]
         self.pos = 0
         self.relbase = 0
         self.running = True
+        if args.verbose:
+            self.verbose = True
+        else:
+            self.verbose = False
+        if args.decode:
+            self.decode = True
+        else:
+            self.decode = False
 
     def decode_argument(self, argument, mode):
-        if args.decode:
+        if self.decode:
             print("\tDecoding argument", argument, "with mode", mode, end=':')
 
         if mode == 0:
-            if args.decode:
+            if self.decode:
                 print("\tPosition mode;", argument, "is an address containing", self.program[argument])
             return( self.program[argument] )
         elif mode == 2:
-            if args.decode:
+            if self.decode:
                 print("\tRelative base mode;", argument, "is an address containing", self.program[argument + self.relbase])
             return( self.program[argument + self.relbase] )
         elif mode == 1:
-            if args.decode:
+            if self.decode:
                 print("\tImmediate mode;", argument, "is a value")
             return( argument ) 
         else:
@@ -35,29 +41,29 @@ class IntCode:
             exit(1)
 
     def do_add(self, modes):
-        if args.verbose:
+        if self.verbose:
             print("\tAdd:", self.program[self.pos:self.pos+4])
         arg_a = self.decode_argument( self.program[self.pos+1], modes[0] )
         arg_b = self.decode_argument( self.program[self.pos+2], modes[1] )
         pos_t = self.program[self.pos+3]
-        if args.verbose:
+        if self.verbose:
             print("\tSum values", arg_a, arg_b, "into", pos_t)
         self.program[pos_t] = arg_a + arg_b
         return( [0, 4] )
 
     def do_mult(self, modes):
-        if args.verbose:
+        if self.verbose:
             print("\tMult:", self.program[self.pos:self.pos+4])
         arg_a = self.decode_argument( self.program[self.pos+1], modes[0] )
         arg_b = self.decode_argument( self.program[self.pos+2], modes[1] )
         pos_t = self.program[self.pos+3]
-        if args.verbose:
+        if self.verbose:
             print("\tMultiply values", arg_a, arg_b, "into", pos_t)
         self.program[pos_t] = arg_a * arg_b
         return( [0, 4] )
 
     def do_input(self, modes):
-        if args.verbose:
+        if self.verbose:
             print("\tInput:", self.program[self.pos:self.pos+2])
         arg_a = self.decode_argument( self.program[self.pos+1], modes[0] )
 
@@ -66,14 +72,14 @@ class IntCode:
             exit(1)
 
         value = self.inputdata.pop(0)
-        if args.verbose:
+        if self.verbose:
             print("\tStoring input value", value, "at location", arg_a)
         self.program[arg_a] = value
 
         return( [0, 2] )
 
     def do_output(self, modes):
-        if args.verbose:
+        if self.verbose:
             print("\tOutput:", self.program[self.pos:self.pos+2])
         arg_a = self.decode_argument( self.program[self.pos+1], modes[0] )
 
@@ -82,73 +88,73 @@ class IntCode:
         return( [0, 2] )
 
     def do_jit(self, modes):
-        if args.verbose:
+        if self.verbose:
             print("\tJump-if-true:", self.program[self.pos:self.pos+3])
         arg_a = self.decode_argument( self.program[self.pos+1], modes[0] )
         arg_b = self.decode_argument( self.program[self.pos+2], modes[1] )
 
         if arg_a != 0:
-            if args.verbose:
+            if self.verbose:
                 print("\t", arg_a, "is nonzero, jumping to", arg_b)
             return([1, arg_b])
         else:
-            if args.verbose:
+            if self.verbose:
                 print("\t", arg_a, "is zero, doing nothing")
             return([0, 3])
 
     def do_jif(self, modes):
-        if args.verbose:
+        if self.verbose:
             print("\tJump-if-false:", self.program[self.pos:self.pos+3])
         arg_a = self.decode_argument( self.program[self.pos+1], modes[0] )
         arg_b = self.decode_argument( self.program[self.pos+2], modes[1] )
 
         if arg_a == 0:
-            if args.verbose:
+            if self.verbose:
                 print("\t", arg_a, "is zero, jumping to", arg_b)
             return([1, arg_b])
         else:
-            if args.verbose:
+            if self.verbose:
                 print("\t", arg_a, "is nonzero, doing nothing")
             return([0, 3])
 
     def do_lt(self, modes):
-        if args.verbose:
+        if self.verbose:
             print("\tLessthan:", self.program[self.pos:self.pos+4])
         arg_a = self.decode_argument( self.program[self.pos+1], modes[0] )
         arg_b = self.decode_argument( self.program[self.pos+2], modes[1] )
         arg_c = self.program[self.pos+3]
 
         if arg_a < arg_b:
-            if args.verbose:
+            if self.verbose:
                 print("\t", arg_a, "is less than", arg_b, "; storing 1 at", arg_c)
             self.program[arg_c] = 1
         else:
-            if args.verbose:
+            if self.verbose:
                 print("\t", arg_a, "is not less than", arg_b, "; storing 0 at", arg_c)
             self.program[arg_c] = 0
 
         return([0, 4])
 
     def do_eq(self, modes):
-        if args.verbose:
+        if self.verbose:
             print("\tEquals:", self.program[self.pos:self.pos+3])
         arg_a = self.decode_argument( self.program[self.pos+1], modes[0] )
         arg_b = self.decode_argument( self.program[self.pos+2], modes[1] )
         arg_c = self.program[self.pos+3]
 
         if arg_a == arg_b:
-            if args.verbose:
+            if self.verbose:
                 print("\t", arg_a, "is equal to", arg_b, "; storing 1 at", arg_c)
             self.program[arg_c] = 1
         else:
-            if args.verbose:
+            if self.verbose:
                 print("\t", arg_a, "is not equal to", arg_b, "; storing 0 at", arg_c)
             self.program[arg_c] = 0
 
         return([0, 4])
 
     def do_rbo(self, modes):
-        if args.verbose:
+        if self.verbose:
             print("\tRelbaseoffset:", self.program[self.pos:self.pos+2])
         arg_a = self.decode_argument( self.program[self.pos+1], modes[0] )
         self.relbase += arg_a
@@ -166,7 +172,7 @@ class IntCode:
                 return(self.output)
 
     def step(self):
-        if args.verbose:
+        if self.verbose:
             print("# Looking for opcode at", self.pos, ":", self.program[self.pos])
 
         opcode = self.program[self.pos] % 100
@@ -205,20 +211,3 @@ class IntCode:
         else:
             print("Unexpected return type", inst_len[0])
             exit(1)
-
-# Parse command-line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("--verbose", help="Print out each instruction as executed", action="store_true")
-parser.add_argument("--decode", help="Print out argument decoding", action="store_true")
-parser.add_argument("filename", help="program source")
-parser.add_argument("input", help="input data", nargs="*", type=int)
-args = parser.parse_args()
-
-inputfile = open(args.filename)
-for line in inputfile:
-    program = [int(x) for x in line.split(',')]
-
-    computer = IntCode(program.copy())
-    output = computer.compute(args.input)
-
-    print("Output:", output)
---
+++ day09/solution.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import argparse
+from itertools import permutations
+import numpy
+import intcode
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--verbose", help="Print out each instruction as executed", action="store_true")
+parser.add_argument("--decode", help="Print out argument decoding", action="store_true")
+parser.add_argument("filename", help="program source")
+parser.add_argument("input", help="input data", nargs="*", type=int)
+args = parser.parse_args()
+
+inputfile = open(args.filename)
+for line in inputfile:
+    program = [int(x) for x in line.split(',')]
+
+    computer = intcode.IntCode(program.copy(), args)
+    output = computer.compute(args.input)
+
+    print("Output:", output)
